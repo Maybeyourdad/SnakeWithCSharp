5a743a5e50c2616564c7064321c7bb4a1c43e641
Maybeyourdad <1027320210@qq.com>
2018-06-28 01:24:15 +0800

    提交

---
+++ Winform贪食蛇/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public SnakeList lst = new SnakeList();
+        private Timer time;
+        private bool b = true;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+
+            time = new Timer();
+            time.Interval = 100;
+            time.Tick += new EventHandler(MoveSnake);
+            lst.form = this;
+
+            lst.CreateSnake();
+            lst.CreateBean();
+            lst.gra = this.CreateGraphics();
+        }
+
+        private void ShowWall()
+        {
+            Graphics p = this.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 2);
+            p.DrawRectangle(pen, 0, 0, 500, 500);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (b == true)
+            {
+                ShowWall();
+                lst.ShowBean();
+                lst.ShowSnake();
+                b = !b;
+            }
+            //Graphics gra = this.CreateGraphics();
+            //gra.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            //Pen pen = new Pen(Color.Black);//画笔颜色      
+            //gra.DrawEllipse(pen, 140, 100, 20, 20);
+            //Invalidate();
+        }
+        
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            base.ProcessCmdKey(ref msg, keyData);
+            switch (keyData)
+            {
+
+                case Keys.Right:
+                    {
+                        if (lst.dt != DirectionType.DT_Left)
+                        {
+                            lst.dt = DirectionType.DT_Right;
+                        }
+                    }
+                    break;
+
+                case Keys.Left:
+                    {
+                        if (lst.dt != DirectionType.DT_Right)
+                        {
+                            lst.dt = DirectionType.DT_Left;
+                        }
+                    }
+                    break;
+
+                case Keys.Up://方向键不反应
+                    {
+                        if (lst.dt != DirectionType.DT_Down)
+                        {
+                            lst.dt = DirectionType.DT_Up;
+                        }
+                    }
+                    break;
+
+                case Keys.Down:
+                    {
+                        if (lst.dt != DirectionType.DT_Up)
+                        {
+                            lst.dt = DirectionType.DT_Down;
+                        }
+                    }
+                    break;
+
+                case Keys.Space:
+                    {
+                        time.Stop();
+                    }
+                    break;
+
+                case Keys.Enter:
+                    {
+                        time.Start();
+                    }
+                    break;
+
+            }
+            return true;
+        }
+
+        public void MoveSnake(object value, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+
+            if (!lst.Eat())
+            {
+                lst.Move();
+            }
+            
+            ShowWall();
+            lst.ShowBean();
+            lst.ShowSnake();
+        }
+    }
+}
---
+++ Winform贪食蛇/WindowsFormsApplication1/SnakeList.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApplication1
+{
+    public enum DirectionType
+    {
+        DT_Right = 0,
+        DT_Left,
+        DT_Up,
+        DT_Down,
+    }
+    public class SnakeList
+    {
+        public Graphics gra { get; set; }
+        public List<Point> list = new List<Point>();
+        Point bean = new Point();
+        private const int nSize = 20;
+        public DirectionType dt { get; set; }
+        public Form form { get; set; }
+        public SnakeList()
+        {
+
+        }
+
+        public void CreateSnake()
+        {
+            for (int i = 0; i < 3; i++ )
+            {
+                Point po = new Point();
+                po.X = i * nSize;
+                po.Y = 0;
+                list.Add(po);
+            }
+        }
+
+        public void CreateBean()
+        {
+            Random rd = new Random();
+            bean.X = rd.Next(0, 25) * nSize;
+            bean.Y = rd.Next(0, 25) * nSize;
+        }
+
+        public void ShowSnake()
+        {
+            foreach (Point po in list)
+            {
+                Pen pen = new Pen(Color.Blue, 2);
+                gra.DrawRectangle(pen, po.X, po.Y, 20, 20);
+            }
+        }
+
+        public void ShowBean()
+        {
+            Pen pen = new Pen(Color.Black, 2);
+            gra.DrawEllipse(pen, bean.X, bean.Y, 20, 20);
+        }
+
+        public bool Move()
+        {
+            Point po = list[0];
+            switch (dt)
+            {
+                case DirectionType.DT_Right:
+                    {
+                        po.X = list[list.Count - 1].X + nSize;
+                        po.Y = list[list.Count - 1].Y;
+                    }
+                    break;
+                case DirectionType.DT_Left:
+                    {
+                        po.X = list[list.Count - 1].X - nSize;
+                        po.Y = list[list.Count - 1].Y;
+                    }
+                    break;
+                case DirectionType.DT_Up:
+                    {
+                        po.X = list[list.Count - 1].X;
+                        po.Y = list[list.Count - 1].Y - nSize;
+                    }
+                    break;
+                case DirectionType.DT_Down:
+                    {
+                        po.X = list[list.Count - 1].X;
+                        po.Y = list[list.Count - 1].Y + nSize;
+                    }
+                    break;
+            }
+            
+            list.Remove(list[0]);
+            list.Add(po);
+            return true;
+        }
+
+        public bool Eat()
+        {
+            if (CanEat())
+            {
+                Point po = new Point();
+                po.X = bean.X;
+                po.Y = bean.Y;
+                list.Add(po);
+
+                CreateBean();
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CanEat()
+        {
+            switch (dt)
+            {
+                case DirectionType.DT_Right:
+                    {
+                        if (list[list.Count - 1].X + 20 == bean.X && list[list.Count - 1].Y == bean.Y)
+                        {
+                            return true;
+                        }
+                    }
+                    break;
+                case DirectionType.DT_Left:
+                    {
+                        if (list[list.Count - 1].X - 20 == bean.X && list[list.Count - 1].Y == bean.Y)
+                        {
+                            return true;
+                        }
+                    }
+                    break;
+                case DirectionType.DT_Up:
+                    {
+                        if (list[list.Count - 1].Y - 20 == bean.Y && list[list.Count - 1].X == bean.X)
+                        {
+                            return true;
+                        }
+                    }
+                    break;
+                case DirectionType.DT_Down:
+                    {
+                        if (list[list.Count - 1].Y + 20 == bean.Y && list[list.Count - 1].X == bean.X)
+                        {
+                            return true;
+                        }
+                    }
+                    break;
+            }
+            return false;
+        }
+    }
+}
